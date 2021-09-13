aaba7e7dfcb9f3a57f0c983a3492e6d6d8528a9a
Anton Palm Folkmann <69316986+antonPalmFolkmann@users.noreply.github.com>
2021-09-13 15:00:02 +0200

    Implemented and tested RegEx.Resolutions (#6)

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -32,5 +32,47 @@ namespace Assignment1.Tests
         var expected = new List<string>(){"Hello", "my", "name", "is", "Caws123"};
         Assert.Equal(expected, output);
         }
+
+        [Fact]
+        public void stringResolutions_convert_1920x1080()
+        {
+        //Given
+        IEnumerable<string> input = new List<string>(){"1920x1080"};
+
+        //When
+        IEnumerable<(int width, int height)> output = RegExpr.Resolution(input);
+
+        //Then
+        IEnumerable<(int width, int height)> expected = new List<(int width, int height)>(){(1920, 1080)};
+        Assert.Equal(expected,output);
+        }
+
+        [Fact]
+        public void stringResolutions_convert_1920x1080_and_1024x768()
+        {
+        //Given
+        IEnumerable<string> input = new List<string>(){"1920x1080, 1024x768"};
+        
+        //When
+        IEnumerable<(int width, int height)> output = RegExpr.Resolution(input);
+        
+        //Then
+        IEnumerable<(int width, int height)> expected = new List<(int width, int height)>(){(1920, 1080), (1024, 768)};
+        Assert.Equal(expected,output);
+        }
+
+        [Fact]
+        public void stringResolutions_convert_8input_to_tuples()
+        {
+        //Given
+        IEnumerable<string> input = new List<string>(){"1920x1080", "1024x768, 800x600, 640x480","320x200, 320x240, 800x600", "1280x960"};
+
+        //When
+        IEnumerable<(int width, int height)> output = RegExpr.Resolution(input);
+        
+        //Then
+        IEnumerable<(int width, int height)> expected = new List<(int width, int height)>(){(1920, 1080), (1024, 768), (800, 600), (640, 480), (320, 200), (320, 240), (800, 600), (1280, 960)};
+        Assert.Equal(expected,output);
+        }
     }
 }

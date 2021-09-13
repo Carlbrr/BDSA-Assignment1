9f5eaa70f4fdcec5fe3cb9e5e106056437f4778b
Carlbrr <90042145+Carlbrr@users.noreply.github.com>
2021-09-13 13:08:58 +0200

    Feature/ex2ITERATORS (#4)
    * updated gitignore to json
    
    * implemented Iterator (TDD)

--- Assignment1/Iterators.cs
+++ Assignment1/Iterators.cs
@@ -7,12 +7,20 @@ namespace Assignment1
     {
         public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items)
         {
-            throw new NotImplementedException();
+            foreach(IEnumerable<T> x in items){
+               foreach(var y in x){
+                   yield return y;
+               } 
+            }
         }
 
         public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate)
         {
-            throw new NotImplementedException();
+            foreach(var x in items){
+                if(predicate(x)==true){
+                    yield return x;
+                }
+            }
         }
     }
 }
